39180eaa6eaa5c2cf740bea8c1858b10b4769af3
Nomi Yu <nomiyu134@gmail.com>
2024-09-16 16:11:33 -0400

    new speech object

--- run_full_pipeline.py
+++ run_full_pipeline.py
@@ -14,92 +14,74 @@ from process import remove_background
 import grab_pictures
 
 use_gui = True
-use_voice = False
+use_voice = True
 do_print = False
 size = 1024
 method = "image_sai_custom"
 client = OpenAI(api_key=api_key)
 
 if use_voice:
-  print(">Initializing. Wait...")
-  print(">Waiting for Hello")
-  speech_playground.wait_for_speech(5)
-  speech_playground.speak_text("Hey, what can I make for you?")
-  main_request = speech_playground.grab_speech(10)
-  print("You requested a " + main_request)
-  speech_playground.speak_text("You wanted a " + main_request + ". You got it!")
+    print(">Initializing. Wait...")
+    speaker = speech_playground.Speaker()
+    speaker.calibrate()
+    print(">Waiting for Hello")
+    speaker.wait_for_speech(60)
+    speaker.speak_text("Hey, what can I make for you?")
+    main_request = speaker.grab_speech(10)
+    print("You requested a " + main_request)
+    speaker.speak_text("You wanted a " + main_request + ". You got it!")
 else:
-  main_request = "cat sitting on bowl of ice cream."
+    main_request = "cat sitting on bowl of ice cream."
 
 # prompt = "a cartoon image of " + main_request + ". do not add background"
 prompt = "a dslr isotropic image of " + main_request + ". do not add background"
-# def request_image(prompt):
-#   print("Fetching image")
-#   image_response = client.images.generate(
-#     model='dall-e-3',
-#     n=1,
-#     size='1024x1024',
-#     prompt=prompt,
-#     response_format="url"
-#   )
-#   print("Retrieving image")
-#   image_url = image_response.data[0].url
-#
-#   img_data = requests.get(image_url).content
-#   image_path = 'dalle_output/dalle_image.png'
-#   with open(image_path, 'wb') as handler:
-#       handler.write(img_data)
-#   # Show the image
-#   img = mpimg.imread(image_path)
-#   plt.imshow(img)
-#   plt.show()
 
 cur_time = datetime.datetime.now()
 save_name = "dalle_" + cur_time.strftime("%d_%m_%H_%M") + "_" + main_request
 img_name = "dalle_output/dalle_image"
 
 def quote(x):
-  return "\"" + x + "\""
+    return "\"" + x + "\""
 
 def generate_3d(version, source_img, size=1024):
-  if version == 1:
-    bashCommand = "python main.py"
-  else:
-    bashCommand = "python main2.py"
-  bashCommand += (" --config configs/" + method + ".yaml input=" + quote(source_img)
-                  + " save_path=" + quote(save_name + "/output") + 'ref_size=' + str(size))
-  if prompt is not None:
-    bashCommand += " prompt=" + quote(prompt)
-  if use_gui:
-    bashCommand += " gui=True"
+    if version == 1:
+        bashCommand = "python main.py"
+    else:
+        bashCommand = "python main2.py"
+    bashCommand += (" --config configs/" + method + ".yaml input=" + quote(source_img)
+                    + " save_path=" + quote(save_name + "/output") + 'ref_size=' + str(size))
+    if prompt is not None:
+        bashCommand += " prompt=" + quote(prompt)
+    if use_gui:
+        bashCommand += " gui=True"
 
-  print("Running:")
-  print(bashCommand)
-  os.system(bashCommand)
+    print("Running:")
+    print(bashCommand)
+    os.system(bashCommand)
 
 if __name__=="__main__":
-  print("> Generating Image")
-  img_path = grab_pictures.request_image(prompt, size=size)
-  img_path = remove_background(img_path, size=size)
-  # bashCommand = "python process.py " + quote(img_name + ".png")
-  # os.system(bashCommand)
+    print("> Generating Image")
+    img_path = grab_pictures.request_image(prompt, size=size)
+    img_path = remove_background(img_path, size=size)
+    # bashCommand = "python process.py " + quote(img_name + ".png")
+    # os.system(bashCommand)
 
-  print("> Processing and generating 3D...")
-  generate_3d(1, source_img=img_path, size=size)
-  generated_output_path = "logs/" + save_name + "/output_mesh.obj"
-  mesh = trimesh.load(generated_output_path)
-  trimesh_util.show_mesh(mesh)
+    print("> Processing and generating 3D...")
+    generate_3d(1, source_img=img_path, size=size)
+    generated_output_path = "logs/" + save_name + "/output_mesh.obj"
+    mesh = trimesh.load(generated_output_path)
+    trimesh_util.show_mesh(mesh)
 
-  print("> Processing to add details")
-  new_mesh = trimesh_obj.modify_obj_with_color(obj_path=generated_output_path,
-                                    edge_weight=0.5, show_progress=False)
-  trimesh_util.show_mesh(new_mesh)
-  processed_output_path = "logs/" + save_name + "/processed_mesh.obj"
-  new_mesh.export(processed_output_path)
+    print("> Processing to add details")
+    new_mesh = trimesh_obj.modify_obj_with_color(obj_path=generated_output_path,
+                                      edge_weight=0.5, show_progress=False)
+    trimesh_util.show_mesh(new_mesh)
+    processed_output_path = "logs/" + save_name + "/processed_mesh.obj"
+    new_mesh.export(processed_output_path)
 
-  if do_print:
-    from form_pipeline import run_preform_slice
+    if do_print:
+      from form_pipeline import run_preform_slice
 
-    print("> Slicing")
-    run_preform_slice(processed_output_path,
-                      output_dir_path="logs/" + save_name+"/", do_print=do_print)
+      print("> Slicing")
+      run_preform_slice(processed_output_path,
+                        output_dir_path="logs/" + save_name+"/", do_print=do_print)
--- speech_playground.py
+++ speech_playground.py
@@ -7,93 +7,104 @@ r = sr.Recognizer()
 class Speaker:
     def __init__(self):
         self.source = sr.Microphone()
+        self.source.__enter__()
         self.r = sr.Recognizer()
 
-    def calibrate(self, duration=0.1):
-        r.adjust_for_ambient_noise(self.source, duration=duration)
+    def calibrate(self, duration=0.5):
+        self.r.adjust_for_ambient_noise(self.source, duration=duration)
 
-    def wait_for_speech(self, max_time=10.0, match_text=None):
-        print("Listening")
+    def wait_for_speech(self, max_time=15.0, match_text=None):
+
         if match_text is None:
-            r.listen(self.source, timeout=max_time)
+            print("Waiting for speech")
+            r.listen(self.source, timeout=max_time, phrase_time_limit=3.0)
         else:
+            print("Waiting for speech: ", match_text)
             received_text = ""
             while received_text.lower() != match_text:
                 received_text = self.grab_speech(max_time=max_time)
+                print("got ", received_text)
 
     def grab_speech(self, max_time=10.0):
         try:
-            audio2 = r.listen(self.source, timeout=max_time)
+            audio2 = r.listen(self.source, timeout=max_time, phrase_time_limit=max_time / 3.0)
             # Using google to recognize audio
             received_text = self.r.recognize_google(audio2)
             received_text = received_text.lower()
-        except sr.RequestError as e:
-            print("Could not request results {0}".format(e))
-        except sr.UnknownValueError:
-            print("unknown error occurred")
+            return received_text
 
-        return received_text
+        except sr.RequestError as e:
+            # print("Could not request results {0}".format(e))
+            return ""
+        except sr.UnknownValueError:
+            # print("unknown error occurred")
+            return ""
+        except sr.WaitTimeoutError:
+            return ""
 
     def speak_text(self, command):
         engine = pyttsx3.init()
         engine.say(command)
         engine.runAndWait()
-
-# Function to convert text to
-# speech
-def speak_text(command):
-    # Initialize the engine
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
-
-def wait_for_speech(max_time = 10.0):
-    try:
-        # use the microphone as source for input.
-        with sr.Microphone() as source2:
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level
-            r.adjust_for_ambient_noise(source2, duration=0.1)
-
-            # listens for the user's input
-            print("Listening")
-            audio2 = r.listen(source2, timeout=max_time)
-
-    except sr.RequestError as e:
-        print("Could not request results {0}".format(e))
-
-    except sr.UnknownValueError:
-        print("unknown error occurred")
-
-def grab_speech(max_time=10.0):
-    try:
-        # use the microphone as source for input.
-        with sr.Microphone() as source2:
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level
-            r.adjust_for_ambient_noise(source2, duration=0.05)
-
-            # listens for the user's input
-            print("Listening")
-            audio2 = r.listen(source2, timeout=max_time)
-
-            print("Converting")
-            # Using google to recognize audio
-            MyText = r.recognize_google(audio2)
-            MyText = MyText.lower()
-    except sr.RequestError as e:
-        print("Could not request results {0}".format(e))
-
-    except sr.UnknownValueError:
-        print("unknown error occurred")
-    return MyText
+#
 
 if __name__=="__main__":
-    while (1):
-        text = grab_speech(10.0)
+    speaker = Speaker()
+    print("calibrating")
+    speaker.calibrate()
+    speaker.speak_text("talk to me")
+    speaker.wait_for_speech(match_text="hey computer")
+    speaker.speak_text("i got it")
 
-        print("Did you say: \n\"", text, "\"")
-        speak_text(text)
 
+
+# # Function to convert text to
+# # speech
+# def speak_text(command):
+#     # Initialize the engine
+#     engine = pyttsx3.init()
+#     engine.say(command)
+#     engine.runAndWait()
+#
+# def wait_for_speech(max_time = 10.0):
+#     try:
+#         # use the microphone as source for input.
+#         with sr.Microphone() as source2:
+#             # wait for a second to let the recognizer
+#             # adjust the energy threshold based on
+#             # the surrounding noise level
+#             r.adjust_for_ambient_noise(source2, duration=0.1)
+#
+#             # listens for the user's input
+#             print("Listening")
+#             audio2 = r.listen(source2, timeout=max_time)
+#
+#     except sr.RequestError as e:
+#         print("Could not request results {0}".format(e))
+#
+#     except sr.UnknownValueError:
+#         print("unknown error occurred")
+#
+# def grab_speech(max_time=10.0):
+#     try:
+#         # use the microphone as source for input.
+#         with sr.Microphone() as source2:
+#             # wait for a second to let the recognizer
+#             # adjust the energy threshold based on
+#             # the surrounding noise level
+#             r.adjust_for_ambient_noise(source2, duration=0.05)
+#
+#             # listens for the user's input
+#             print("Listening")
+#             audio2 = r.listen(source2, timeout=max_time)
+#
+#             print("Converting")
+#             # Using google to recognize audio
+#             MyText = r.recognize_google(audio2)
+#             MyText = MyText.lower()
+#     except sr.RequestError as e:
+#         print("Could not request results {0}".format(e))
+#
+#     except sr.UnknownValueError:
+#         print("unknown error occurred")
+#     return MyText
